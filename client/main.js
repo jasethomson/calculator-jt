9b508fb9a9a9baae16de9bdeeec6010e63fe5a50
Jase Thomson <jasethomson31@gmail.com>
2019-07-10 20:37:40 -0700

    Calculate Inputs - Jase Thomson

--- client/main.js
+++ client/main.js
@@ -28,16 +28,36 @@ function operatorButtonHandler(event){
   calculationArray.push(stringNumberToPush);
   calculationArray.push(inputtedOperatorValue);
   stringNumberToPush="";
-  console.log(calculationArray);
 }
 function equalsButtonHandler(event){
-  console.log('event object: ', event);
   calculationArray.push(stringNumberToPush);
   stringNumberToPush ="";
   displayArray = [];
-  console.log(calculationArray);
+  var answer = calculate(calculationArray[0],calculationArray[2],calculationArray[1]);
+  displayArray.push(answer);
+  updateDisplay();
 }
 function updateDisplay(){
   var displayText = displayArray.join("");
   $('#display-text').text(displayText);
+}
+function calculate(num1,num2,operator){
+  var number1 = parseFloat(num1);
+  var number2 = parseFloat(num2);
+  var result = null;
+  switch(operator){
+    case '+':
+      result = number1 + number2;
+    break;
+    case '-':
+      result = number1 - number2;
+    break;
+    case '*':
+      result = number1 * number2;
+    break;
+    case '/':
+      result = number1 / number2;
+    break;
+  }
+  return result;
 }
